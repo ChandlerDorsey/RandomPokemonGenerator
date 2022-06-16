88c85db793a6b77e4ccda7aed409f9d6c6d2b9d8
ChandlerDorsey <107429205+ChandlerDorsey@users.noreply.github.com>
2022-06-16 18:47:56 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+const pokemongenerated = document.getElementById('pokemongenerated');
+
+const fetchPokemon = () => {
+const array = [];
+const randomID = Math.floor(Math.random() * 906) + 1;
+
+
+    const url = `https://pokeapi.co/api/v2/pokemon/${randomID}`;
+    array.push(fetch(url).then((res) => res.json()));
+
+Promise.all(array).then((results) => {
+     const pokemon = results.map((data) => ({
+        name: data.name,
+        image: data.sprites['front_default'],
+        type: data.types.map((type) => type.type.name).join(', '),
+        typeCss: data.types.map((type)=>type.type.name).join('--'),
+        id: data.id,
+        
+       
+
+    }));
+    displayPokemon(pokemon);
+  });
+ };
+const displayPokemon = (pokemon) => {
+console.log(pokemon);
+const pokemonHTMLString = pokemon
+    .map(
+        (pokemon) => `
+    <ul class="card pokemon-type type-${pokemon.typeCss}">
+        <img class="card-image" src="${pokemon.image}"/>
+        <h2 class="card-name"> National No ${pokemon.id}. ${pokemon.name}</h2>
+        <p class="card-type">Type: ${pokemon.type}</p>        
+
+    </ul>
+`
+    )
+    .join('');
+
+document.querySelector('#pokemonGenerated').innerHTML = pokemonHTMLString;
+};
+
+fetchPokemon();
